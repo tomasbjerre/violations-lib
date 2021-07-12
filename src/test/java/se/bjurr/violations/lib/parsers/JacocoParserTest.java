aa07d906c30661ebe1eabce56c55e3bea233944e
Tomas Bjerre <tomas.bjerre85@gmail.com>
2021-07-12 21:35:20 +0200

    chore: moving jacoco test-files ot their own files

--- src/test/java/se/bjurr/violations/lib/parsers/JacocoParserTest.java
+++ src/test/java/se/bjurr/violations/lib/parsers/JacocoParserTest.java
@@ -1,167 +1,182 @@
 package se.bjurr.violations.lib.parsers;
 
-import static org.hamcrest.CoreMatchers.is;
-import static org.junit.Assert.assertThat;
+import static org.assertj.core.api.Assertions.assertThat;
+import static se.bjurr.violations.lib.TestUtils.getRootFolder;
+import static se.bjurr.violations.lib.ViolationsApi.violationsApi;
+import static se.bjurr.violations.lib.reports.Parser.JACOCO;
 
+import java.util.ArrayList;
 import java.util.Set;
-import java.util.logging.Level;
-import org.approvaltests.Approvals;
 import org.junit.Test;
-import se.bjurr.violations.lib.ViolationsLogger;
+import se.bjurr.violations.lib.model.SEVERITY;
 import se.bjurr.violations.lib.model.Violation;
 
 public class JacocoParserTest {
 
   @Test
   public void shouldSkipViolationUnderLineThreshold() throws Exception {
-    String report =
-        "<report name=\"violations-lib\">\n"
-            + "  <package name=\"se/bjurr/violations/lib/reports\">\n"
-            + "    <class name=\"se/bjurr/violations/lib/reports/Parser\" sourcefilename=\"Parser.java\">\n"
-            + "      <method name=\"findViolations\"\n"
-            + "        desc=\"(Lse/bjurr/violations/lib/ViolationsLogger;Ljava/util/List;)Ljava/util/Set;\"\n"
-            + "        line=\"71\">\n"
-            + "        <counter type=\"INSTRUCTION\" missed=\"52\" covered=\"40\"/>\n"
-            + "        <counter type=\"LINE\" missed=\"1\" covered=\"2\"/>\n"
-            + "      </method>\n"
-            + "    </class>\n"
-            + "  </package>\n"
-            + "</report>\n";
-    Set<Violation> violations = new JacocoParser().parseReportOutput(report, new NullLogger());
-    assertThat(violations.size(), is(0));
+    final String rootFolder = getRootFolder();
+
+    final Set<Violation> actual =
+        violationsApi() //
+            .withPattern(".*/jacoco/shouldSkipViolationUnderLineThreshold\\.xml$") //
+            .inFolder(rootFolder) //
+            .findAll(JACOCO) //
+            .violations();
+
+    assertThat(actual) //
+        .hasSize(0);
   }
 
   @Test
   public void shouldSkipViolationOverCoverageThreshold() throws Exception {
-    String report =
-        "<report name=\"violations-lib\">\n"
-            + "  <package name=\"se/bjurr/violations/lib/reports\">\n"
-            + "    <class name=\"se/bjurr/violations/lib/reports/Parser\" sourcefilename=\"Parser.java\">\n"
-            + "      <method name=\"findViolations\"\n"
-            + "        desc=\"(Lse/bjurr/violations/lib/ViolationsLogger;Ljava/util/List;)Ljava/util/Set;\"\n"
-            + "        line=\"71\">\n"
-            + "        <counter type=\"INSTRUCTION\" missed=\"30\" covered=\"70\"/>\n"
-            + "        <counter type=\"LINE\" missed=\"6\" covered=\"9\"/>\n"
-            + "      </method>\n"
-            + "    </class>\n"
-            + "  </package>\n"
-            + "</report>\n";
-    Set<Violation> violations = new JacocoParser().parseReportOutput(report, new NullLogger());
-    assertThat(violations.size(), is(0));
+    final String rootFolder = getRootFolder();
+
+    final Set<Violation> actual =
+        violationsApi() //
+            .withPattern(".*/jacoco/shouldSkipViolationOverCoverageThreshold\\.xml$") //
+            .inFolder(rootFolder) //
+            .findAll(JACOCO) //
+            .violations();
+
+    assertThat(actual) //
+        .hasSize(0);
   }
 
   @Test
   public void shouldParseViolation() throws Exception {
-    String report =
-        "<report name=\"violations-lib\">\n"
-            + "  <package name=\"se/bjurr/violations/lib/reports\">\n"
-            + "    <class name=\"se/bjurr/violations/lib/reports/Parser\" sourcefilename=\"Parser.java\">\n"
-            + "      <method name=\"findViolations\"\n"
-            + "        desc=\"(Lse/bjurr/violations/lib/ViolationsLogger;Ljava/util/List;)Ljava/util/Set;\"\n"
-            + "        line=\"71\">\n"
-            + "        <counter type=\"INSTRUCTION\" missed=\"52\" covered=\"40\"/>\n"
-            + "        <counter type=\"LINE\" missed=\"6\" covered=\"9\"/>\n"
-            + "      </method>\n"
-            + "    </class>\n"
-            + "  </package>\n"
-            + "</report>\n";
-    Set<Violation> violations = new JacocoParser().parseReportOutput(report, new NullLogger());
-    Approvals.verifyAll("violations", violations);
+    final String rootFolder = getRootFolder();
+
+    final Set<Violation> actual =
+        violationsApi() //
+            .withPattern(".*/jacoco/shouldParseViolation\\.xml$") //
+            .inFolder(rootFolder) //
+            .findAll(JACOCO) //
+            .violations();
+
+    assertThat(actual) //
+        .hasSize(1);
+
+    final Violation violation0 = new ArrayList<>(actual).get(0);
+    assertThat(violation0.getMessage()) //
+        .isEqualTo(
+            "Covered 40 out of 92 instructions (43,48%) for findViolations(Lse/bjurr/violations/lib/ViolationsLogger;Ljava/util/List;)Ljava/util/Set;");
+    assertThat(violation0.getFile()) //
+        .isEqualTo("se/bjurr/violations/lib/reports/Parser.java");
+    assertThat(violation0.getSeverity()) //
+        .isEqualTo(SEVERITY.WARN);
+    assertThat(violation0.getStartLine()) //
+        .isEqualTo(71);
   }
 
   @Test
   public void shouldParseViolationsInMultipleMethods() throws Exception {
-    String report =
-        "<report name=\"violations-lib\">\n"
-            + "  <package name=\"se/bjurr/violations/lib/reports\">\n"
-            + "    <class name=\"se/bjurr/violations/lib/reports/Parser\" sourcefilename=\"Parser.java\">\n"
-            + "      <method name=\"findViolations\"\n"
-            + "        desc=\"(Lse/bjurr/violations/lib/ViolationsLogger;Ljava/util/List;)Ljava/util/Set;\"\n"
-            + "        line=\"71\">\n"
-            + "        <counter type=\"INSTRUCTION\" missed=\"52\" covered=\"40\"/>\n"
-            + "        <counter type=\"LINE\" missed=\"6\" covered=\"9\"/>\n"
-            + "      </method>\n"
-            + "      <method name=\"findViolations2\"\n"
-            + "        desc=\"(Lse/bjurr/violations/lib/ViolationsLogger;Ljava/util/List;)Ljava/util/Set;\"\n"
-            + "        line=\"101\">\n"
-            + "        <counter type=\"INSTRUCTION\" missed=\"50\" covered=\"42\"/>\n"
-            + "        <counter type=\"LINE\" missed=\"6\" covered=\"9\"/>\n"
-            + "      </method>\n"
-            + "    </class>\n"
-            + "  </package>\n"
-            + "</report>\n";
-    Set<Violation> violations = new JacocoParser().parseReportOutput(report, new NullLogger());
-    Approvals.verifyAll("violations", violations);
+    final String rootFolder = getRootFolder();
+
+    final Set<Violation> actual =
+        violationsApi() //
+            .withPattern(".*/jacoco/shouldParseViolationsInMultipleMethods\\.xml$") //
+            .inFolder(rootFolder) //
+            .findAll(JACOCO) //
+            .violations();
+
+    assertThat(actual) //
+        .hasSize(2);
+
+    final Violation violation0 = new ArrayList<>(actual).get(0);
+    assertThat(violation0.getMessage()) //
+        .isEqualTo(
+            "Covered 42 out of 92 instructions (45,65%) for findViolations2(Lse/bjurr/violations/lib/ViolationsLogger;Ljava/util/List;)Ljava/util/Set;");
+    assertThat(violation0.getFile()) //
+        .isEqualTo("se/bjurr/violations/lib/reports/Parser.java");
+    assertThat(violation0.getSeverity()) //
+        .isEqualTo(SEVERITY.WARN);
+    assertThat(violation0.getStartLine()) //
+        .isEqualTo(101);
+
+    final Violation violation1 = new ArrayList<>(actual).get(0);
+    assertThat(violation1.getMessage()) //
+        .isEqualTo(
+            "Covered 42 out of 92 instructions (45,65%) for findViolations2(Lse/bjurr/violations/lib/ViolationsLogger;Ljava/util/List;)Ljava/util/Set;");
+    assertThat(violation1.getFile()) //
+        .isEqualTo("se/bjurr/violations/lib/reports/Parser.java");
+    assertThat(violation1.getSeverity()) //
+        .isEqualTo(SEVERITY.WARN);
+    assertThat(violation1.getStartLine()) //
+        .isEqualTo(101);
   }
 
   @Test
   public void shouldParseViolationsInMultipleClasses() throws Exception {
-    String report =
-        "<report name=\"violations-lib\">\n"
-            + "  <package name=\"se/bjurr/violations/lib/reports\">\n"
-            + "    <class name=\"se/bjurr/violations/lib/reports/Parser\" sourcefilename=\"Parser.java\">\n"
-            + "      <method name=\"findViolations\"\n"
-            + "        desc=\"(Lse/bjurr/violations/lib/ViolationsLogger;Ljava/util/List;)Ljava/util/Set;\"\n"
-            + "        line=\"71\">\n"
-            + "        <counter type=\"INSTRUCTION\" missed=\"52\" covered=\"40\"/>\n"
-            + "        <counter type=\"LINE\" missed=\"6\" covered=\"9\"/>\n"
-            + "      </method>\n"
-            + "    </class>\n"
-            + "    <class name=\"se/bjurr/violations/lib/reports/Parser2\" sourcefilename=\"Parser2.java\">\n"
-            + "      <method name=\"findViolations\"\n"
-            + "        desc=\"(Lse/bjurr/violations/lib/ViolationsLogger;Ljava/util/List;)Ljava/util/Set;\"\n"
-            + "        line=\"71\">\n"
-            + "        <counter type=\"INSTRUCTION\" missed=\"50\" covered=\"42\"/>\n"
-            + "        <counter type=\"LINE\" missed=\"6\" covered=\"9\"/>\n"
-            + "      </method>\n"
-            + "    </class>\n"
-            + "  </package>\n"
-            + "</report>\n";
-    Set<Violation> violations = new JacocoParser().parseReportOutput(report, new NullLogger());
-    Approvals.verifyAll("violations", violations);
+    final String rootFolder = getRootFolder();
+
+    final Set<Violation> actual =
+        violationsApi() //
+            .withPattern(".*/jacoco/shouldParseViolationsInMultipleClasses\\.xml$") //
+            .inFolder(rootFolder) //
+            .findAll(JACOCO) //
+            .violations();
+
+    assertThat(actual) //
+        .hasSize(2);
+
+    final Violation violation0 = new ArrayList<>(actual).get(0);
+    assertThat(violation0.getMessage()) //
+        .isEqualTo(
+            "Covered 40 out of 92 instructions (43,48%) for findViolations(Lse/bjurr/violations/lib/ViolationsLogger;Ljava/util/List;)Ljava/util/Set;");
+    assertThat(violation0.getFile()) //
+        .isEqualTo("se/bjurr/violations/lib/reports/Parser.java");
+    assertThat(violation0.getSeverity()) //
+        .isEqualTo(SEVERITY.WARN);
+    assertThat(violation0.getStartLine()) //
+        .isEqualTo(71);
+
+    final Violation violation1 = new ArrayList<>(actual).get(1);
+    assertThat(violation1.getMessage()) //
+        .isEqualTo(
+            "Covered 42 out of 92 instructions (45,65%) for findViolations(Lse/bjurr/violations/lib/ViolationsLogger;Ljava/util/List;)Ljava/util/Set;");
+    assertThat(violation1.getFile()) //
+        .isEqualTo("se/bjurr/violations/lib/reports/Parser2.java");
+    assertThat(violation1.getSeverity()) //
+        .isEqualTo(SEVERITY.WARN);
+    assertThat(violation1.getStartLine()) //
+        .isEqualTo(71);
   }
 
   @Test
   public void shouldParseViolationsInMultipleGroups() throws Exception {
-    String report =
-        "<report name=\"violations-lib\">\n"
-            + "  <group name=\"group1\">\n"
-            + "    <package name=\"se/bjurr/violations/lib/reports\">\n"
-            + "      <class name=\"se/bjurr/violations/lib/reports/Parser\" sourcefilename=\"Parser.java\">\n"
-            + "        <method name=\"findViolations\"\n"
-            + "          desc=\"(Lse/bjurr/violations/lib/ViolationsLogger;Ljava/util/List;)Ljava/util/Set;\"\n"
-            + "          line=\"71\">\n"
-            + "          <counter type=\"INSTRUCTION\" missed=\"52\" covered=\"40\"/>\n"
-            + "          <counter type=\"LINE\" missed=\"6\" covered=\"9\"/>\n"
-            + "        </method>\n"
-            + "      </class>\n"
-            + "    </package>\n"
-            + "  </group>\n"
-            + "  <group name=\"group2\">\n"
-            + "    <group name=\"group3\">\n"
-            + "      <package name=\"se/bjurr/violations/lib/reports\">\n"
-            + "        <class name=\"se/bjurr/violations/lib/reports/Parser2\" sourcefilename=\"Parser2.java\">\n"
-            + "          <method name=\"findViolations\"\n"
-            + "            desc=\"(Lse/bjurr/violations/lib/ViolationsLogger;Ljava/util/List;)Ljava/util/Set;\"\n"
-            + "            line=\"71\">\n"
-            + "            <counter type=\"INSTRUCTION\" missed=\"50\" covered=\"42\"/>\n"
-            + "            <counter type=\"LINE\" missed=\"6\" covered=\"9\"/>\n"
-            + "           </method>\n"
-            + "         </class>\n"
-            + "       </package>\n"
-            + "    </group>\n"
-            + "  </group>\n"
-            + "</report>\n";
-    Set<Violation> violations = new JacocoParser().parseReportOutput(report, new NullLogger());
-    Approvals.verifyAll("violations", violations);
-  }
+    final String rootFolder = getRootFolder();
 
-  private static class NullLogger implements ViolationsLogger {
+    final Set<Violation> actual =
+        violationsApi() //
+            .withPattern(".*/jacoco/shouldParseViolationsInMultipleGroups\\.xml$") //
+            .inFolder(rootFolder) //
+            .findAll(JACOCO) //
+            .violations();
 
-    @Override
-    public void log(Level level, String string) {}
+    assertThat(actual) //
+        .hasSize(2);
 
-    @Override
-    public void log(Level level, String string, Throwable t) {}
+    final Violation violation0 = new ArrayList<>(actual).get(0);
+    assertThat(violation0.getMessage()) //
+        .isEqualTo(
+            "Covered 40 out of 92 instructions (43,48%) for findViolations(Lse/bjurr/violations/lib/ViolationsLogger;Ljava/util/List;)Ljava/util/Set;");
+    assertThat(violation0.getFile()) //
+        .isEqualTo("se/bjurr/violations/lib/reports/Parser.java");
+    assertThat(violation0.getSeverity()) //
+        .isEqualTo(SEVERITY.WARN);
+    assertThat(violation0.getStartLine()) //
+        .isEqualTo(71);
+
+    final Violation violation1 = new ArrayList<>(actual).get(1);
+    assertThat(violation1.getMessage()) //
+        .isEqualTo(
+            "Covered 42 out of 92 instructions (45,65%) for findViolations(Lse/bjurr/violations/lib/ViolationsLogger;Ljava/util/List;)Ljava/util/Set;");
+    assertThat(violation1.getFile()) //
+        .isEqualTo("se/bjurr/violations/lib/reports/Parser2.java");
+    assertThat(violation1.getSeverity()) //
+        .isEqualTo(SEVERITY.WARN);
+    assertThat(violation1.getStartLine()) //
+        .isEqualTo(71);
   }
 }
