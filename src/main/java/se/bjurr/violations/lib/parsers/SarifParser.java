ac8ae5e6694d7cb3dbe3e3740835fc84f699c66c
Tomas Bjerre <tomas.bjerre85@gmail.com>
2022-08-24 20:20:27 +0200

    feat(SARIF): supporting category (refs #155)

--- src/main/java/se/bjurr/violations/lib/parsers/SarifParser.java
+++ src/main/java/se/bjurr/violations/lib/parsers/SarifParser.java
@@ -3,20 +3,21 @@ package se.bjurr.violations.lib.parsers;
 import static se.bjurr.violations.lib.model.Violation.violationBuilder;
 import static se.bjurr.violations.lib.util.Utils.isNullOrEmpty;
 
-import java.lang.reflect.Type;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
-import java.util.TreeSet;
-
 import com.google.gson.GsonBuilder;
 import com.google.gson.JsonDeserializationContext;
 import com.google.gson.JsonDeserializer;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonParseException;
-
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.Set;
+import java.util.TreeSet;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 import se.bjurr.violations.lib.ViolationsLogger;
 import se.bjurr.violations.lib.model.SEVERITY;
 import se.bjurr.violations.lib.model.Violation;
@@ -24,6 +25,7 @@ import se.bjurr.violations.lib.model.generated.sarif.Artifact;
 import se.bjurr.violations.lib.model.generated.sarif.Location;
 import se.bjurr.violations.lib.model.generated.sarif.Message;
 import se.bjurr.violations.lib.model.generated.sarif.PhysicalLocation;
+import se.bjurr.violations.lib.model.generated.sarif.PropertyBag;
 import se.bjurr.violations.lib.model.generated.sarif.Region;
 import se.bjurr.violations.lib.model.generated.sarif.ReportingDescriptor;
 import se.bjurr.violations.lib.model.generated.sarif.Result;
@@ -64,6 +66,7 @@ public class SarifParser implements ViolationsParser {
       return violations;
     }
     for (final Run run : report.getRuns()) {
+      final Map<String, ReportingDescriptor> rulesById = this.getRulesById(run);
       String reporter = "Sarif";
       final boolean isToolSet = run.getTool() != null && run.getTool().getDriver() != null;
       if (isToolSet
@@ -72,9 +75,7 @@ public class SarifParser implements ViolationsParser {
         reporter = run.getTool().getDriver().getName();
       }
       final List<Artifact> artifacts = new ArrayList<>(run.getArtifacts());
-      final Map<String, String> helpMap = this.extractHelpText(run);
       for (final Result result : run.getResults()) {
-
         final String ruleId = result.getRuleId();
         final String message = this.extractMessage(result.getMessage());
         if (isNullOrEmpty(message)) {
@@ -82,6 +83,14 @@ public class SarifParser implements ViolationsParser {
         }
         final Level level = result.getLevel();
 
+        String category = null;
+        if (rulesById.get(ruleId) != null) {
+          final PropertyBag properties = rulesById.get(ruleId).getProperties();
+          if (properties != null && properties.getCategory() != null) {
+            category = properties.getCategory().toString();
+          }
+        }
+        final Optional<String> helpTextOpt = this.findHelpText(rulesById.get(ruleId));
         final List<Location> locations = result.getLocations();
         final boolean hasLocations = locations != null && locations.size() > 0;
         if (hasLocations) {
@@ -107,10 +116,8 @@ public class SarifParser implements ViolationsParser {
             if (regionMessage != null) {
               fullMessage.append("\n\n").append(regionMessage);
             }
-            if (helpMap.containsKey(ruleId)) {
-              fullMessage
-                  .append("\n\nFor additional help see: ")
-                  .append(this.getRuleHelpOrId(helpMap, ruleId));
+            if (helpTextOpt.isPresent()) {
+              fullMessage.append("\n\nFor additional help see: ").append(helpTextOpt.get());
             }
             violations.add(
                 violationBuilder()
@@ -121,6 +128,7 @@ public class SarifParser implements ViolationsParser {
                     .setMessage(fullMessage.toString().trim())
                     .setSeverity(this.toSeverity(level))
                     .setReporter(reporter)
+                    .setCategory(category)
                     .build());
           }
         } else {
@@ -130,21 +138,15 @@ public class SarifParser implements ViolationsParser {
                   .setFile(Violation.NO_FILE)
                   .setStartLine(Violation.NO_LINE)
                   .setRule(ruleId)
-                  .setMessage(this.getRuleHelpOrId(helpMap, ruleId))
+                  .setMessage(helpTextOpt.orElse(ruleId))
                   .setSeverity(this.toSeverity(level))
                   .setReporter(reporter)
+                  .setCategory(category)
                   .build());
         }
       }
     }
     return violations;
-  }
-
-  private String getRuleHelpOrId(final Map<String, String> helpMap, final String ruleId) {
-    if (helpMap.containsKey(ruleId)) {
-      return helpMap.get(ruleId);
-    }
-    return ruleId;
   }
 
   /**
@@ -164,28 +166,31 @@ public class SarifParser implements ViolationsParser {
     return text;
   }
 
-  private Map<String, String> extractHelpText(final Run run) {
-    final Map<String, String> helpMap = new HashMap<>();
+  private Map<String, ReportingDescriptor> getRulesById(final Run run) {
     if (run.getTool() != null
         && run.getTool().getDriver() != null
         && run.getTool().getDriver().getRules() != null) {
-      for (final ReportingDescriptor r : run.getTool().getDriver().getRules()) {
-        if (r.getHelp() != null && !isNullOrEmpty(r.getHelp().getMarkdown())) {
-          helpMap.put(r.getId(), r.getHelp().getMarkdown());
-        } else if (r.getHelp() != null && !isNullOrEmpty(r.getHelp().getText())) {
-          helpMap.put(r.getId(), r.getHelp().getText());
-        } else if (r.getFullDescription() != null
-            && !isNullOrEmpty(r.getFullDescription().getMarkdown())) {
-          helpMap.put(r.getId(), r.getFullDescription().getMarkdown());
-        } else if (r.getFullDescription() != null
-            && !isNullOrEmpty(r.getFullDescription().getText())) {
-          helpMap.put(r.getId(), r.getFullDescription().getText());
-        } else if (!isNullOrEmpty(r.getName())) {
-          helpMap.put(r.getId(), r.getName());
-        }
-      }
+      return run.getTool().getDriver().getRules().stream()
+          .collect(Collectors.toMap(ReportingDescriptor::getId, Function.identity()));
     }
-    return helpMap;
+    return new HashMap<>();
+  }
+
+  private Optional<String> findHelpText(final ReportingDescriptor r) {
+    if (r == null) {
+      return Optional.empty();
+    }
+    if (r.getHelp() != null && !isNullOrEmpty(r.getHelp().getMarkdown())) {
+      return Optional.ofNullable(r.getHelp().getMarkdown());
+    } else if (r.getHelp() != null && !isNullOrEmpty(r.getHelp().getText())) {
+      return Optional.ofNullable(r.getHelp().getText());
+    } else if (r.getFullDescription() != null
+        && !isNullOrEmpty(r.getFullDescription().getMarkdown())) {
+      return Optional.ofNullable(r.getFullDescription().getMarkdown());
+    } else if (r.getFullDescription() != null && !isNullOrEmpty(r.getFullDescription().getText())) {
+      return Optional.ofNullable(r.getFullDescription().getText());
+    }
+    return Optional.ofNullable(r.getName());
   }
 
   private SEVERITY toSeverity(final Level level) {
--- src/test/java/se/bjurr/violations/lib/SarifParserTest.java
+++ src/test/java/se/bjurr/violations/lib/SarifParserTest.java
@@ -164,13 +164,87 @@ public class SarifParserTest {
         .isEqualTo(Violation.NO_FILE);
     assertThat(first.getSeverity()) //
         .isEqualTo(SEVERITY.ERROR);
+  }
 
-    final String severities =
-        actual.stream()
-            .map(Violation::getSeverity)
-            .map(it -> it.name())
-            .collect(Collectors.joining(","));
-    assertThat(severities) //
-        .isEqualTo("ERROR");
+  @Test
+  public void testThatViolationsCanBeParsed_with_category() {
+    final String rootFolder = getRootFolder();
+
+    final Set<Violation> actual =
+        violationsApi() //
+            .withPattern(".*/sarif/with-category.json$") //
+            .inFolder(rootFolder) //
+            .findAll(SARIF) //
+            .violations();
+
+    assertThat(actual) //
+        .hasSize(6);
+    final List<Violation> arrayList = new ArrayList<>(actual);
+
+    final Violation violation0 = arrayList.get(0);
+    assertThat(violation0.getMessage()) //
+        .isEqualTo(
+            "The Common Language Specification (CLS) defines naming restrictions, data types, and rules to which assemblies must conform if they will be used across programming languages. Good design dictates that all assemblies explicitly indicate CLS compliance by using CLSCompliantAttribute . If this attribute is not present on an assembly, the assembly is not compliant.");
+    assertThat(violation0.getFile()) //
+        .isEqualTo(Violation.NO_FILE);
+    assertThat(violation0.getSeverity()) //
+        .isEqualTo(SEVERITY.WARN);
+    assertThat(violation0.getCategory()) //
+        .isEqualTo("Design");
+
+    final Violation violation1 = arrayList.get(1);
+    assertThat(violation1.getMessage()) //
+        .isEqualToIgnoringWhitespace(
+            "Exception type System.Exception is not sufficiently specific For additional help see: An exception of type that is not sufficiently specific or reserved by the runtime should never be raised by user code. This makes the original error difficult to detect and debug. If this exception instance might be thrown, use a different exception type.");
+    assertThat(violation1.getFile()) //
+        .isEqualTo("file:///C:/Projects/SarifCategoryDemo/Class1.cs");
+    assertThat(violation1.getSeverity()) //
+        .isEqualTo(SEVERITY.WARN);
+    assertThat(violation1.getCategory()) //
+        .isEqualTo("Usage");
+
+    final Violation violation2 = arrayList.get(2);
+    assertThat(violation2.getMessage()) //
+        .isEqualToIgnoringWhitespace(
+            "The behavior of 'string.Format(string, object)' could vary based on the current user's locale settings. Replace this call in 'Class1.M(string)' with a call to 'string.Format(IFormatProvider, string, params object[])'. For additional help see: A method or constructor calls one or more members that have overloads that accept a System.IFormatProvider parameter, and the method or constructor does not call the overload that takes the IFormatProvider parameter. When a System.Globalization.CultureInfo or IFormatProvider object is not supplied, the default value that is supplied by the overloaded member might not have the effect that you want in all locales. If the result will be based on the input from/output displayed to the user, specify 'CultureInfo.CurrentCulture' as the 'IFormatProvider'. Otherwise, if the result will be stored and accessed by software, such as when it is loaded from disk/database and when it is persisted to disk/database, specify 'CultureInfo.InvariantCulture'.");
+    assertThat(violation2.getFile()) //
+        .isEqualTo("file:///C:/Projects/SarifCategoryDemo/Class1.cs");
+    assertThat(violation2.getSeverity()) //
+        .isEqualTo(SEVERITY.WARN);
+    assertThat(violation2.getCategory()) //
+        .isEqualTo("Globalization");
+
+    final Violation violation3 = arrayList.get(3);
+    assertThat(violation3.getMessage()) //
+        .isEqualToIgnoringWhitespace(
+            "'string.Contains(string)' has a method overload that takes a 'StringComparison' parameter. Replace this call in 'SarifCategoryDemo.Class1.M(string)' with a call to 'string.Contains(string, System.StringComparison)' for clarity of intent. For additional help see: A string comparison operation uses a method overload that does not set a StringComparison parameter. It is recommended to use the overload with StringComparison parameter for clarity of intent. If the result will be displayed to the user, such as when sorting a list of items for display in a list box, specify 'StringComparison.CurrentCulture' or 'StringComparison.CurrentCultureIgnoreCase' as the 'StringComparison' parameter. If comparing case-insensitive identifiers, such as file paths, environment variables, or registry keys and values, specify 'StringComparison.OrdinalIgnoreCase'. Otherwise, if comparing case-sensitive identifiers, specify 'StringComparison.Ordinal'.");
+    assertThat(violation3.getFile()) //
+        .isEqualTo("file:///C:/Projects/SarifCategoryDemo/Class1.cs");
+    assertThat(violation3.getSeverity()) //
+        .isEqualTo(SEVERITY.WARN);
+    assertThat(violation3.getCategory()) //
+        .isEqualTo("Globalization");
+
+    final Violation violation4 = arrayList.get(4);
+    assertThat(violation4.getMessage()) //
+        .isEqualToIgnoringWhitespace(
+            "Use 'string.Contains(char)' instead of 'string.Contains(string)' when searching for a single character For additional help see: 'string.Contains(char)' is available as a better performing overload for single char lookup.");
+    assertThat(violation4.getFile()) //
+        .isEqualTo("file:///C:/Projects/SarifCategoryDemo/Class1.cs");
+    assertThat(violation4.getSeverity()) //
+        .isEqualTo(SEVERITY.WARN);
+    assertThat(violation4.getCategory()) //
+        .isEqualTo("Performance");
+
+    final Violation violation5 = arrayList.get(5);
+    assertThat(violation5.getMessage()) //
+        .isEqualToIgnoringWhitespace(
+            "Member 'M' does not access instance data and can be marked as static For additional help see: Members that do not access instance data or call instance methods can be marked as static. After you mark the methods as static, the compiler will emit nonvirtual call sites to these members. This can give you a measurable performance gain for performance-sensitive code.");
+    assertThat(violation5.getFile()) //
+        .isEqualTo("file:///C:/Projects/SarifCategoryDemo/Class1.cs");
+    assertThat(violation5.getSeverity()) //
+        .isEqualTo(SEVERITY.WARN);
+    assertThat(violation5.getCategory()) //
+        .isEqualTo("Performance");
   }
 }
