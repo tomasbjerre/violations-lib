dbf0279053ab0098e39336fa445e7c26e76b5f4e
Tomas Bjerre <tomas.bjerre85@gmail.com>
2023-01-15 12:26:14 +0100

    fix: relative paths to root in sarif export

--- src/main/java/se/bjurr/violations/lib/model/ViolationUtils.java
+++ src/main/java/se/bjurr/violations/lib/model/ViolationUtils.java
@@ -1,6 +1,5 @@
 package se.bjurr.violations.lib.model;
 
-import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
@@ -12,9 +11,12 @@ import java.util.Optional;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
+import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
+
 public final class ViolationUtils {
   private ViolationUtils() {}
 
+  @SuppressFBWarnings("PATH_TRAVERSAL_IN")
   public static String relativePath(final Violation v) {
     final String userDir = System.getProperty("user.dir");
     final String file = v.getFile();
