2426dc9da28e7a6dd972b9d29f88c55abc3fae25
Tomas Bjerre <tomas.bjerre85@gmail.com>
2023-01-19 17:31:17 +0100

    chore: cleanup of SemgrepParser before merge of PR

--- src/main/java/se/bjurr/violations/lib/parsers/SemgrepParser.java
+++ src/main/java/se/bjurr/violations/lib/parsers/SemgrepParser.java
@@ -4,19 +4,26 @@ import static se.bjurr.violations.lib.model.Violation.violationBuilder;
 import static se.bjurr.violations.lib.reports.Parser.SEMGREP;
 
 import com.google.gson.Gson;
-import java.util.*;
+import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
+import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.Set;
 import se.bjurr.violations.lib.ViolationsLogger;
 import se.bjurr.violations.lib.model.SEVERITY;
 import se.bjurr.violations.lib.model.Violation;
 
 public class SemgrepParser implements ViolationsParser {
 
+  @SuppressFBWarnings({
+    "UWF_UNWRITTEN_PUBLIC_OR_PROTECTED_FIELD",
+    "NP_UNWRITTEN_PUBLIC_OR_PROTECTED_FIELD"
+  })
   @Override
-  public Set<Violation> parseReportOutput(String reportContent, ViolationsLogger violationsLogger)
-      throws Exception {
+  public Set<Violation> parseReportOutput(
+      final String reportContent, final ViolationsLogger violationsLogger) throws Exception {
     final Set<Violation> violations = new LinkedHashSet<>();
     final SemgrepReport report = new Gson().fromJson(reportContent, SemgrepReport.class);
-    for (SemgrepResult result : report.results) {
+    for (final SemgrepResult result : report.results) {
       if (!result.extra.is_ignored) {
         violations.add(
             violationBuilder()
@@ -28,64 +35,56 @@ public class SemgrepParser implements ViolationsParser {
                 .setFile(result.path)
                 .setSeverity(result.extra.getSeverity())
                 .setRule(result.check_id)
-                .setMessage(String.format("%s\n\n<p>%s</p>", result.extra.metadata.source, result.extra.message))
+                .setMessage(
+                    String.format(
+                        "%s%n%n<p>%s</p>", result.extra.metadata.source, result.extra.message))
                 .build());
       }
     }
     return violations;
   }
 
+  @SuppressFBWarnings("NP_UNWRITTEN_PUBLIC_OR_PROTECTED_FIELD")
   private static class SemgrepReport {
-    private String version;
-    private List<SemgrepResult> results;
+    public List<SemgrepResult> results;
   }
 
+  @SuppressFBWarnings("NP_UNWRITTEN_PUBLIC_OR_PROTECTED_FIELD")
   private static class SemgrepResult {
-    private String check_id;
-    private Location end;
-    private Extra extra;
-    private String path;
-    private Location start;
+    public String check_id;
+    public Location end;
+    public Extra extra;
+    public String path;
+    public Location start;
   }
 
+  @SuppressFBWarnings("NP_UNWRITTEN_PUBLIC_OR_PROTECTED_FIELD")
   private static class Extra {
-    private String fingerprint;
-    private boolean is_ignored;
-    private String lines;
-    private String message;
-    private Metadata metadata;
-    private String severity;
+    public boolean is_ignored;
+    public String message;
+    public Metadata metadata;
+    public String severity;
 
     public SEVERITY getSeverity() {
-      switch (severity) {
-        case "INFO":
-          return SEVERITY.INFO;
+      switch (this.severity) {
         case "WARNING":
           return SEVERITY.WARN;
         case "ERROR":
           return SEVERITY.ERROR;
         default:
-          return null;
+          return SEVERITY.INFO;
       }
     }
   }
 
+  @SuppressFBWarnings("NP_UNWRITTEN_PUBLIC_OR_PROTECTED_FIELD")
   private static class Location {
-    private int col;
-    private int line;
-    private int offset;
+    public int col;
+    public int line;
   }
 
-  private class Metadata{
-    private String category;
-    private String confidence;
-    private ArrayList<String> cwe;
-    private String impact;
-    private String license;
-    private String likelihood;
-    private ArrayList<String> owasp;
-    private ArrayList<String> references;
-    private String shortlink;
-    private String source;
+  @SuppressFBWarnings("NP_UNWRITTEN_PUBLIC_OR_PROTECTED_FIELD")
+  private static class Metadata {
+    public String source;
   }
 }
